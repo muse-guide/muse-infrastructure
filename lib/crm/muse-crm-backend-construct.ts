6acc1d6b69447caf7aea05ade6777b5ce9a68105
Michał Pacholczyk <32038667+michalpacholczyk@users.noreply.github.com>
2024-01-02 20:10:18 +0100

    Add catch

--- lib/crm/muse-crm-backend-construct.ts
+++ lib/crm/muse-crm-backend-construct.ts
@@ -1,6 +1,6 @@
 import {Construct} from "constructs";
 import * as cdk from "aws-cdk-lib";
-import {RemovalPolicy} from "aws-cdk-lib";
+import {Duration, RemovalPolicy} from "aws-cdk-lib";
 import * as lambdaNode from "aws-cdk-lib/aws-lambda-nodejs";
 import * as lambda from "aws-cdk-lib/aws-lambda";
 import * as step from "aws-cdk-lib/aws-stepfunctions";
@@ -17,6 +17,7 @@ export interface MuseCrmBackendConstructProps extends cdk.StackProps {
 
 export class MuseCrmBackendConstruct extends Construct {
 
+    public readonly errorHandlerLambda: lambdaNode.NodejsFunction
     public readonly crmQrCodeGeneratorLambda: lambdaNode.NodejsFunction
     public readonly crmAssetProcessorLambda: lambdaNode.NodejsFunction
     public readonly crmGetExhibitionLambda: lambdaNode.NodejsFunction
@@ -31,10 +32,17 @@ export class MuseCrmBackendConstruct extends Construct {
     constructor(scope: Construct, id: string, props: MuseCrmBackendConstructProps) {
         super(scope, id);
 
+        // Error handler
+        this.errorHandlerLambda = new lambdaNode.NodejsFunction(this, "ErrorHandlerLambdaLambda", {
+            functionName: `crm-${props.envName}-error-handler-lambda`,
+            runtime: lambda.Runtime.NODEJS_20_X,
+            entry: path.join(__dirname, "../../../muse-crm-server/src/error.handler.ts")
+        });
+
         // QR code generator
         this.crmQrCodeGeneratorLambda = new lambdaNode.NodejsFunction(this, "CrmQrCodeGeneratorLambda", {
             functionName: `crm-${props.envName}-qr-code-generator-lambda`,
-            runtime: lambda.Runtime.NODEJS_18_X,
+            runtime: lambda.Runtime.NODEJS_20_X,
             entry: path.join(__dirname, "../../../muse-crm-server/src/qr-code.generator.ts"),
             environment: {
                 APP_DOMAIN: "https://muse.cloud",
@@ -47,7 +55,7 @@ export class MuseCrmBackendConstruct extends Construct {
         // Asset processor
         this.crmAssetProcessorLambda = new lambdaNode.NodejsFunction(this, "CrmAssetProcessorLambda", {
             functionName: `crm-${props.envName}-asset-processor-lambda`,
-            runtime: lambda.Runtime.NODEJS_18_X,
+            runtime: lambda.Runtime.NODEJS_20_X,
             entry: path.join(__dirname, "../../../muse-crm-server/src/asset.processor.ts"),
             environment: {
                 CRM_ASSET_BUCKET: props.crmStorage.crmAssetBucket.bucketName,
@@ -62,7 +70,7 @@ export class MuseCrmBackendConstruct extends Construct {
         // Get Exhibition lambda
         this.crmGetExhibitionLambda = new lambdaNode.NodejsFunction(this, "CrmGetExhibitionLambda", {
             functionName: `crm-${props.envName}-get-exhibition-lambda`,
-            runtime: lambda.Runtime.NODEJS_18_X,
+            runtime: lambda.Runtime.NODEJS_20_X,
             entry: path.join(__dirname, "../../../muse-crm-server/src/exhibition-get.handler.ts"),
             environment: {
                 EXHIBITION_TABLE_NAME: props.crmStorage.crmExhibitionTable.tableName
@@ -78,7 +86,7 @@ export class MuseCrmBackendConstruct extends Construct {
         // Get Exhibitions lambda
         this.crmGetExhibitionsLambda = new lambdaNode.NodejsFunction(this, "CrmGetExhibitionsLambda", {
             functionName: `crm-${props.envName}-get-exhibitions-lambda`,
-            runtime: lambda.Runtime.NODEJS_18_X,
+            runtime: lambda.Runtime.NODEJS_20_X,
             entry: path.join(__dirname, "../../../muse-crm-server/src/exhibition-get-all.handler.ts"),
             environment: {
                 EXHIBITION_TABLE_NAME: props.crmStorage.crmExhibitionTable.tableName
@@ -94,7 +102,7 @@ export class MuseCrmBackendConstruct extends Construct {
         // Create Exhibition lambda
         this.crmCreateExhibitionLambda = new lambdaNode.NodejsFunction(this, "CrmCreateExhibitionLambda", {
             functionName: `crm-${props.envName}-create-exhibition-lambda`,
-            runtime: lambda.Runtime.NODEJS_18_X,
+            runtime: lambda.Runtime.NODEJS_20_X,
             entry: path.join(__dirname, "../../../muse-crm-server/src/exhibition-create.handler.ts"),
             environment: {
                 EXHIBITION_TABLE_NAME: props.crmStorage.crmExhibitionTable.tableName,
@@ -117,7 +125,7 @@ export class MuseCrmBackendConstruct extends Construct {
         // Delete Exhibition lambda
         this.crmDeleteExhibitionLambda = new lambdaNode.NodejsFunction(this, "CrmDeleteExhibitionLambda", {
             functionName: `crm-${props.envName}-delete-exhibition-lambda`,
-            runtime: lambda.Runtime.NODEJS_18_X,
+            runtime: lambda.Runtime.NODEJS_20_X,
             entry: path.join(__dirname, "../../../muse-crm-server/src/exhibition-delete.handler.ts"),
             environment: {
                 EXHIBITION_TABLE_NAME: props.crmStorage.crmExhibitionTable.tableName,
@@ -137,10 +145,18 @@ export class MuseCrmBackendConstruct extends Construct {
             })
         );
 
+        const commonRetry: cdk.aws_stepfunctions.RetryProps = {
+            maxAttempts: 3,
+            backoffRate: 2,
+            interval: Duration.seconds(1)
+        }
+
+        const commonCatch = new step.Pass(this, "CatchAllState")
+
         // Update Exhibition lambda
         this.crmUpdateExhibitionLambda = new lambdaNode.NodejsFunction(this, "CrmUpdateExhibitionLambda", {
             functionName: `crm-${props.envName}-update-exhibition-lambda`,
-            runtime: lambda.Runtime.NODEJS_18_X,
+            runtime: lambda.Runtime.NODEJS_20_X,
             entry: path.join(__dirname, "../../../muse-crm-server/src/exhibition-update.handler.ts"),
             environment: {
                 EXHIBITION_TABLE_NAME: props.crmStorage.crmExhibitionTable.tableName,
@@ -174,26 +190,45 @@ export class MuseCrmBackendConstruct extends Construct {
                 level: step.LogLevel.ALL,
                 includeExecutionData: true,
             },
-            definition: new tasks.LambdaInvoke(this, "CreateExhibition",
+            definitionBody: step.DefinitionBody.fromChainable(new tasks.LambdaInvoke(this, "CreateExhibition",
                 {
                     lambdaFunction: this.crmCreateExhibitionLambda,
                     outputPath: '$.Payload',
                 })
+                .addRetry(commonRetry)
+                .addCatch(new tasks.LambdaInvoke(this, "CreateExhibitionErrorHandler",
+                    {
+                        lambdaFunction: this.errorHandlerLambda,
+                        outputPath: '$.Payload',
+                    })
+                    .addRetry(commonRetry)
+                    .next(new step.Fail(this, "CreateExhibitionFail",
+                            {
+                                errorPath: step.JsonPath.stringAt('$.error'),
+                                causePath: step.JsonPath.stringAt('$.cause'),
+                            }
+                        )
+                    )
+                )
                 .next(new tasks.LambdaInvoke(this, "GenerateQrCode",
                     {
                         lambdaFunction: this.crmQrCodeGeneratorLambda,
                         outputPath: '$.Payload',
-                    }
-                ))
+                    })
+                    .addRetry(commonRetry)
+                )
                 .next(new tasks.LambdaInvoke(this, "CreateExhibitionProcessAsset",
                     {
                         lambdaFunction: this.crmAssetProcessorLambda,
                         outputPath: '$.Payload',
-                    }
-                ))
-                .next(new step.Succeed(this, "Created"))
+                    })
+                    .addRetry(commonRetry)
+                )
+                .next(new step.Succeed(this, "Created", {
+                    outputPath: '$.mutation.entityId',
+                }))
+            )
         });
-
 
         // Update Exhibition Step Function
         const updateExhibitionLogGroup = new LogGroup(this, 'CrmUpdateExhibitionLogGroup', {
@@ -209,18 +244,35 @@ export class MuseCrmBackendConstruct extends Construct {
                 level: step.LogLevel.ALL,
                 includeExecutionData: true,
             },
-            definition: new tasks.LambdaInvoke(this, "UpdateExhibition",
+            definitionBody: step.DefinitionBody.fromChainable(new tasks.LambdaInvoke(this, "UpdateExhibition",
                 {
                     lambdaFunction: this.crmUpdateExhibitionLambda,
                     outputPath: '$.Payload',
                 })
+                .addRetry(commonRetry)
+                .addCatch(new tasks.LambdaInvoke(this, "UpdateExhibitionErrorHandler",
+                    {
+                        lambdaFunction: this.errorHandlerLambda,
+                        outputPath: '$.Payload',
+                    })
+                    .addRetry(commonRetry)
+                    .next(new step.Fail(this, "UpdateExhibitionFail",
+                            {
+                                errorPath: step.JsonPath.stringAt('$.error'),
+                                causePath: step.JsonPath.stringAt('$.cause'),
+                            }
+                        )
+                    )
+                )
                 .next(new tasks.LambdaInvoke(this, "UpdateExhibitionProcessAsset",
                     {
                         lambdaFunction: this.crmAssetProcessorLambda,
                         outputPath: '$.Payload',
-                    }
-                ))
+                    })
+                    .addRetry(commonRetry)
+                )
                 .next(new step.Succeed(this, "Updated"))
+            )
         });
 
         // Delete Exhibition Step Function
@@ -237,18 +289,35 @@ export class MuseCrmBackendConstruct extends Construct {
                 level: step.LogLevel.ALL,
                 includeExecutionData: true,
             },
-            definition: new tasks.LambdaInvoke(this, "DeleteExhibition",
+            definitionBody: step.DefinitionBody.fromChainable(new tasks.LambdaInvoke(this, "DeleteExhibition",
                 {
                     lambdaFunction: this.crmDeleteExhibitionLambda,
                     outputPath: '$.Payload',
                 })
+                .addRetry(commonRetry)
+                .addCatch(new tasks.LambdaInvoke(this, "DeleteExhibitionErrorHandler",
+                    {
+                        lambdaFunction: this.errorHandlerLambda,
+                        outputPath: '$.Payload',
+                    })
+                    .addRetry(commonRetry)
+                    .next(new step.Fail(this, "DeleteExhibitionFail",
+                            {
+                                errorPath: step.JsonPath.stringAt('$.error'),
+                                causePath: step.JsonPath.stringAt('$.cause'),
+                            }
+                        )
+                    )
+                )
                 .next(new tasks.LambdaInvoke(this, "DeleteExhibitionProcessAsset",
                     {
                         lambdaFunction: this.crmAssetProcessorLambda,
                         outputPath: '$.Payload',
-                    }
-                ))
+                    })
+                    .addRetry(commonRetry)
+                )
                 .next(new step.Succeed(this, "Deleted"))
+            )
         });
     }
 }
